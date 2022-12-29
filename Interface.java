467fb5eca98b76c41f89d7ecf96c4ab1db0db2d6
J Prem <88187995+Prem-Jain@users.noreply.github.com>
2022-12-29 15:35:07 +0530

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,39 @@
+package COE;
+
+interface Shape4
+{
+	double area(int s);
+}
+class Squareeee implements Shape4
+{
+	public double area(int s)
+	{
+		System.out.println("Area method of Square Class");
+		return s * s;
+	}
+}
+class Cubeee extends Squareeee
+{
+	public double area(int s)
+	{
+		System.out.println("Area method of Cube Class");
+		return s * super.area(s);
+	}
+}
+class Circleee implements Shape4
+{
+	public double area(int r)
+	{
+		System.out.println("Area method of Circle Class");
+		return 3.14 * r * r;
+	}
+}
+
+public class Interface {
+	public static void main(String[] args) {
+		Cubeee c = new Cubeee();
+		System.out.println("Volumn of Cube = " + c.area(10));
+		Circleee cl = new Circleee();
+		System.out.println("Area of Circle = " + cl.area(1));
+	}
+}
