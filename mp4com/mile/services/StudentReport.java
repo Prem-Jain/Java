e21a94769ca55f3abb974c641e92a42191507578
J Prem <88187995+Prem-Jain@users.noreply.github.com>
2023-01-05 16:20:18 +0530

    Add files via upload

---
+++ mp4com/mile/services/StudentReport.java
@@ -0,0 +1,32 @@
+package mp4com.mile.services;
+
+import mp4com.mile.bean.*;
+import mp4com.mile.exceptions.*;
+
+public class StudentReport {
+	public String findGrades(Student sObj)
+	{
+		int m[] = sObj.getMarks();
+		if(m[0] < 35 || m[1] < 35 || m[2] < 35)
+			return "F";
+		int sum = m[0] + m[1] + m[2];
+		if(sum < 150)
+			return "C";
+		if(sum < 200)
+			return "B";
+		if(sum < 250)
+			return "A";
+		return "A+";
+	}
+	
+	public String validate(Student s) throws NullNameException, NullMarksArrException, NullStudentObjectException
+	{
+		if(s == null)
+			throw new NullStudentObjectException();
+		if(s.getName() == null)
+			throw new NullNameException();
+		if(s.getMarks() == null)
+			throw new NullMarksArrException();
+		return "Valid";
+	}
+}
---
+++ mp4com/mile/services/StudentService.java
@@ -0,0 +1,38 @@
+package mp4com.mile.services;
+
+import mp4com.mile.bean.*;
+
+public class StudentService {
+	public int findNumberOfNullMarksArray(Student sArr[])
+	{
+		int count = 0;
+		for(Student s : sArr)
+		{
+			if(s != null)
+				if(s.getMarks() == null)
+					count++;
+		}
+		return count;
+	}
+	public int findNumberOfNullNames(Student sArr[])
+	{
+		int count = 0;
+		for(Student s : sArr)
+		{
+			if(s != null)
+				if(s.getName() == null)
+					count++;
+		}
+		return count;
+	}
+	public int findNumberOfNullObjects(Student sArr[])
+	{
+		int count = 0;
+		for(Student s : sArr)
+		{
+			if(s == null)
+				count++;
+		}
+		return count;
+	}
+}
